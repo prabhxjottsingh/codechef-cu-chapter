40de58f188099f653f6c5767a3eb166c595623a4
Prabhxjott <prabhx.jott@gmail.com>
2022-04-01 17:10:02 +0530

    FLOW016 - Practise

---
+++ Semester 4/Week 02/Practise/GCDandLCM.py
@@ -0,0 +1,17 @@
+def get_array():
+    return list(map(int, input().split()))
+
+
+def findGCD(a, b):
+    if(b == 0):
+        return a
+    else:
+        return findGCD(b, a % b)
+
+
+numberOfTestCases = int(input())
+for i in range(numberOfTestCases):
+    array = get_array()
+    gcdOfTwoElements = findGCD(array[1], array[0])
+    hcfOfTwoElements = (int)(array[1]*array[0]/gcd);
+    print(gcdOfTwoElements, hcfOfTwoElements)
